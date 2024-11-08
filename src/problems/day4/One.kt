fb3da8c0a674bc3bc0712f6e381e015d9fec9165
goodwillparking <48186345+goodwillparking@users.noreply.github.com>
2024-11-07 20:03:23 -0500

    Solve problem 4 part 2.

--- src/problems/day4/One.kt
+++ src/problems/day4/One.kt
@@ -10,6 +10,7 @@ private object One {
     @JvmStatic
     fun main(args: Array<String>) {
         println("part1: ${part1()}")
+        println("part2: ${part2()}")
     }
 
     private fun part1(): Int = getInput("day4/one")
@@ -19,6 +20,24 @@ private object One {
         .map(Card::points)
         .sum()
 
+    private fun part2(): Int {
+         val cards = getInput("day4/one")
+             .lineSequence()
+             .filter { it.isNotBlank() }
+             .map(::parseCard)
+             .toList()
+
+        return cardCount(cards, cards, 0)
+    }
+
+    fun cardCount(cards: List<Card>, allCards: List<Card>, offset: Int): Int {
+        return cards.asSequence().mapIndexed { i, card ->
+            val wins = card.wins
+            val copyOffset = offset + i + 1
+            val copies = allCards.subList(copyOffset, copyOffset + wins)
+            cardCount(copies, allCards, copyOffset) + 1
+        }.sum()
+    }
 
     fun parseCard(line: String): Card {
         val result = regex.find(line)!!
@@ -41,5 +60,7 @@ private object One {
                 else -> 2.0.pow(matches - 1).toInt()
             }
         }
+
+        val wins by lazy { winningValues.intersect(values).size }
     }
 }
